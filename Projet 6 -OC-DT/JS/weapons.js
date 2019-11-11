724efa3e6aa42698efe06bfcf993885a3aeeb6a9
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2019-11-11 16:21:25 +0100

    Add files via upload
    Final

--- Projet 6 -OC-DT/JS/weapons.js
+++ Projet 6 -OC-DT/JS/weapons.js
@@ -1,54 +1,38 @@
 class weapons {
-  constructor(id,damage,name) {
+  constructor(id = 0) {
     this.id = id;
-    this.damage = damage;
-    this.name = name;
-    /* switch (this.id) {
-
-      case 0:
-        this.damage = 16;
-        this.name = "axe";
-        break;
-
-      case 1:
-        this.damage = 12;
-        this.name = "dagger";
-        break;
-
-      case 2:
-        this.damage = 14;
-        this.name = "spear";
-        break;
-
-      case 3:
-        this.damage = 13;
-        this.name = "flail";
-        break;
-
-      case 4:
-        this.damage = 15;
-        this.name = "longsword";
-        break;
-
-      case 5:
-        this.damage = 17;
-        this.name = "crystalsword";
-        break;
-
-      default:
-        this.damage = 10;
-        this.name = "sword";
-        break;
-    }; */
+    this.damage = weapons.armes[id].damage;
+    this.name = weapons.armes[id].name;
   };
 };
-weapons.armes = [
-  '1, 16, axe',
-  '2, 12, dagger',
-  '3, 14, spear',
-  '4, 13, flail',
-  '5, 15, longsword',
-  '6, 17, crystalsword',
-  '0, 10, sword'
+weapons.armes = [ 
+{
+  damage : 10,
+  name : 'sword'
+},
+{
+  damage : 16,
+  name : 'axe'
+},
+{
+  damage : 12,
+  name : 'dagger'
+},
+{
+  damage : 14,
+  name : 'spear'
+},
+{
+  damage : 13,
+  name : 'flail'
+},
+{
+  damage : 15,
+  name : 'longsword'
+},
+{
+  damage : 17,
+  name : 'crystalsword'
+}
 ]
 
