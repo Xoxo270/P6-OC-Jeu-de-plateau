fe459ac734f335f2b742b9708067aac450f8e87f
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2023-10-18 18:12:11 +0200

    Add files via upload

---
+++ Jeu de plateau/JS/global.js
@@ -0,0 +1,9 @@
+/* Déclaration des variables globales */
+
+let currentPlayer;
+let tableauJoueurs;
+let movementFight;
+let board = 10;
+let walls = 10;
+let porteeDeplacement = 3;
+let nbArmes = 4;
