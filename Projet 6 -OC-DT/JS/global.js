7f6d7479a7bfe24a0a0665a985ca2934b5bdf1dc
Xoxo270 <47757884+Xoxo270@users.noreply.github.com>
2023-10-18 18:07:13 +0200

    Delete Projet 6 -OC-DT directory

--- Projet 6 -OC-DT/JS/global.js
+++
@@ -1,9 +0,0 @@
-/* Déclaration des variables globales */
-
-let currentPlayer;
-let tableauJoueurs;
-let movementFight;
-let board = 10;
-let walls = 10;
-let porteeDeplacement = 3;
-let nbArmes = 4;
